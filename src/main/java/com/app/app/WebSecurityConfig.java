592a6fad82c74e4fae72841124a49f178e4b6ecb
pedro <pedro@t2s.com.br>
2020-11-02 22:43:57 -0300

    [add] add logout e página de login

--- src/main/java/com/app/app/WebSecurityConfig.java
+++ src/main/java/com/app/app/WebSecurityConfig.java
@@ -23,7 +23,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.formLogin(form -> form 
 					.loginPage("/login")
 					.permitAll()
-				);
+				)
+				.logout(logout -> logout.logoutUrl("/logout"));
 	}
 
 	@Bean
